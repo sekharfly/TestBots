f491dcd8e198d2e72d6edfefc969a5eb6744362f
sekharfly <sekharfly@gmail.com>
2018-09-21 11:58:47 +0530

    first commit

---
+++ botforxenvoice/src/main/java/com/bots/demo/BotforxenvoiceApplication.java
@@ -0,0 +1,12 @@
+package com.bots.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotforxenvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotforxenvoiceApplication.class, args);
+	}
+}
